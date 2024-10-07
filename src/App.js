25f898fcde478cfac17f1b9a5b1de42738c67920
Sahil Deore <sahildeore@gmail.com>
2024-10-08 00:11:44 +0530

    Moved Board to separate component file

--- src/App.js
+++ src/App.js
@@ -1,60 +1,4 @@
-import { useState } from 'react'
-import Square from './Square';
-
-function Board() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [value, setValue] = useState("X");
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else if (!squares.includes(null)){
-    status = "Draw";
-  } else {
-    status = "Next player: " + (value);
-  }
-
-  function handleClick(index) {
-    if(squares[index] || calculateWinner(squares)) {
-      return;
-    }
-    
-    switchValue();
-    const nextSquares = squares.slice();
-    nextSquares[index] = value;
-    setSquares(nextSquares);
-    //console.log("clicked! " + value);
-  }
-
-  function switchValue() {
-    value == "X" ? setValue("O") : setValue("X");
-    //console.log("switching");
-  }
-
-  return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
-    </div>);
-}
+import Board from './Board';
 
 export default function Game() {
   return (
@@ -62,24 +6,4 @@ export default function Game() {
       <Board />
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
