c199d31ab15e4f9a396549f3351f1404453a864f
Sahil Deore <sahildeore@gmail.com>
2024-10-09 13:24:57 +0530

    added board to online page

--- src/Online.js
+++ src/Online.js
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+import Board from './Board';
+
 export default function Online() {
+
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(index) {
+        console.log("TODO: Handle API Calling here by updating " +  index);
+    }
+
     return (
         <div>
             <div>Online Game</div>
-            COMING SOON!
+            <Board squares={squares} handleClick={handleClick}/>
         </div>
 
     );
